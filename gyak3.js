e2d8b1674abf08be6a62c2d8c686a268413103a2
Mike5163 <egeresi.mihaly2@gmail.com>
2024-03-20 21:11:23 +0100

    feladatok készen

--- gyak3.js
+++ gyak3.js
@@ -9,8 +9,35 @@ const car = {
 }
 
 
-const fruit = {
-    
+// Access and print the color of your dream car.
+console.log("Car model:", car["color"]);
+
+// Modify the year of your dream car to 2025.
+
+car.year = 2025;
+console.log("Updated car details:", car);
+
+
+// Add a new property to your dream car object representing its horsepower.
+car.price = 25000;
+console.log("Car with price:", car);
+
+//  Delete the property representing the fuel type from your dream car object.
+ delete car.price;
+ console.log("Car without price property:", car);
+
+// Create an object representing your favorite book with properties for title, author, and number of pages.
+
+const book = {
+    author: "J. R. R. Tolkien",
+    title: "A Gyűrűk Ura I-III",
+    number_of_pages: 1774,
 }
 
-//egefdha
+// Access and print the title of your favorite book.
+console.log(book)
+
+// Modify the author of your favorite book to your favorite author.
+
+book.author = "Bán Mór"
+console.log("Book with author:", book);
