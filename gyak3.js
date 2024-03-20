d501c985b5d323f40812defd5b9b79dae1647a9e
Mike5163 <egeresi.mihaly2@gmail.com>
2024-03-20 20:46:49 +0100

    first commit

---
+++ gyak3.js
@@ -0,0 +1,16 @@
+// Create an object representing your dream car
+
+const car = {
+    brand: "Vokswagen",
+    model: "Id 3",
+    year: 2022,
+    color: "blue",
+    isElectric: true
+}
+
+
+const fruit = {
+    
+}
+
+//egefdha
