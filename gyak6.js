7c8c13f552d06584dd0e8535f722e6be4cbdbab1
Mike5163 <egeresi.mihaly2@gmail.com>
2024-04-01 11:20:02 +0200

    update new file

--- gyak6.js
+++ gyak6.js
@@ -1,3 +1,12 @@
-// proba
+// Write an if-else statement to check if 20.24 is positive, negative, or zero.
 
-//:)
+const number = 20.24;
+
+if(number >0){
+    console.log(number + " positive")
+} else if (number < 0){
+    console.log(number + " negative")
+}
+else{
+    console.log(number + " zero")
+}
